aa5b7422a02dd151873142f71c9020d68a30f47a
sharad9 <sharadrakeshm@gmail.com>
2022-05-29 22:38:09 +0530

    make it better

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
+import com.example.demo.global.GlobalData;
 import com.example.demo.service.CategoryService;
 import com.example.demo.service.ProductService;
 
@@ -19,12 +19,12 @@ public class HomeController {
 	
 	@GetMapping({"/","/home"})
 	public String home(Model model) {
-		//model.addAttribute("cartCount",GlobalData.cart.size());
+		model.addAttribute("cartCount",GlobalData.cart.size());
 		return "index";
 	}
 	@GetMapping("/shop")
 	public String shop(Model model) {
-		//model.addAttribute("cartCount",GlobalData.cart.size());
+		model.addAttribute("cartCount",GlobalData.cart.size());
 		model.addAttribute("categories",categoryService.getAllCategory());
 		model.addAttribute("products",productService.getAllProduct());
 		
@@ -34,7 +34,7 @@ public class HomeController {
 	@GetMapping("/shop/category/{id}")
 	
 	public String shopByCategory(Model model, @PathVariable int id) {
-		//model.addAttribute("cartCount",GlobalData.cart.size());
+		model.addAttribute("cartCount",GlobalData.cart.size());
 		model.addAttribute("categories",categoryService.getAllCategory());
 		model.addAttribute("products",productService.getAllProductsByCategoryId((id)));
 		
@@ -43,7 +43,8 @@ public class HomeController {
 	@GetMapping("/shop/viewproduct/{id}")
 	public String viewProduct(Model model, @PathVariable int id) {
 		
-		//model.addAttribute("cartCount",GlobalData.cart.size());
+
+		model.addAttribute("cartCount",GlobalData.cart.size());
 		model.addAttribute("product",productService.getProductById(id).get());
 		
 		return "viewProduct";
